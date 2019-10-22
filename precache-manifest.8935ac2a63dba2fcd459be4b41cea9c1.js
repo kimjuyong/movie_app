8accd30d84ed072c8fbe55083700be56b2e69008
juyongkim <kimwtgr@gmail.com>
2019-10-23 00:36:23 +0900

    Updates

---
+++ precache-manifest.8935ac2a63dba2fcd459be4b41cea9c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0287a2a9d7033d2cddfc6e35c75ea80",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "fca4065feeccee042711",
+    "url": "/movie_app/static/css/main.806ce0ca.chunk.css"
+  },
+  {
+    "revision": "5315c5f9153bb1ca4995",
+    "url": "/movie_app/static/js/2.e04721d9.chunk.js"
+  },
+  {
+    "revision": "fca4065feeccee042711",
+    "url": "/movie_app/static/js/main.4aca7666.chunk.js"
+  },
+  {
+    "revision": "8971f8699b511f3e262b",
+    "url": "/movie_app/static/js/runtime-main.2890315e.js"
+  }
+]);
